161dcb11be083dbce1167fe0f8b5c401eaa7344b
Загир Валитов <zagir-valitov@outlook.com>
2024-10-02 14:26:04 +0500

    Добавьте файлы проекта.

---
+++ ImageDownloader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Basic_09_L030_Mod_1;
+
+internal class ImageDownloader
+{
+    //public delegate void DownloadHandler(string message);
+    //public event DownloadHandler? ImageStartedNotify;
+    //public event DownloadHandler? ImageCompletedNotify;
+    public event Action<string>? ImageStartedNotify;
+    public event Action<string>? ImageCompletedNotify;
+    public event Action<string>? FileAvailableNotify;
+    public event Action<string>? FileNotAvailableNotify;
+    public static int Count = 1;
+    public List<string> DownloadList = [];
+    public List<string> UrlList = [];
+    public bool UrlListIsOpen;
+    
+    public async Task DownloadAsync(string uri, string fileName)
+    {
+        fileName = $"{fileName}_{Count++}.jpg";
+        DownloadList.Add(fileName);
+        ImageStartedNotify?.Invoke($"File {fileName}\tdownloaded started!");
+        using (var myWebClient = new WebClient())
+        {
+            await myWebClient.DownloadFileTaskAsync(uri, fileName);
+        }
+        ImageCompletedNotify?.Invoke($"File {fileName}\tdownload completed!");        
+    }
+
+    public List<string> GetUrlListFromTxtFile(string puth)
+    {
+        //UrlList = new List<string>();
+
+        if (File.Exists(puth))
+        {
+            UrlListIsOpen = true;
+            FileAvailableNotify?.Invoke("URL list file available");
+            using (StreamReader reader = new StreamReader(puth))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    UrlList.Add(line);
+                }
+            }
+        }
+        else
+        {
+            UrlListIsOpen = false;
+            FileNotAvailableNotify?.Invoke("URL list file unavailable!");
+        }
+        return UrlList;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using HomeWork_Basic_09_L030_Mod_1;
+using System.IO;
+using System.Text;
+
+Console.WriteLine(" ---- Home work 9 Mod 1 ----\n");
+
+var imageDownloader = new ImageDownloader();
+imageDownloader.ImageStartedNotify += DisplayStartedMessage;
+imageDownloader.ImageCompletedNotify += DisplayCompletedMessage;
+imageDownloader.FileAvailableNotify += DisplayFileAvailable;
+imageDownloader.FileNotAvailableNotify += DisplayFileNotAvailable;
+
+CancellationTokenSource cts = new CancellationTokenSource();
+CancellationToken token = cts.Token;
+
+var fileName = "picture";
+var urlList = imageDownloader.GetUrlListFromTxtFile(@"C:\Users\Загир\source\repos\OTUS\HomeWorksBasic\HomeWork_Basic_09_L030_Mod_1\UrlList.txt");
+var taskList = new List<Task>();
+
+foreach(var url in urlList)
+{
+    taskList.Add(imageDownloader.DownloadAsync(url, fileName));
+}
+
+
+while (imageDownloader.UrlListIsOpen)
+{
+    Console.WriteLine("\nPress \"A\" to exit or any other key to check the download status\n");
+    
+    var command = Console.ReadLine();
+    if (command == "A")
+    {
+        cts.Cancel();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("\n Operation interrupted");
+        Console.WriteLine("\n ------ !Exit! ------");
+        Console.ResetColor();
+        break;
+    }
+
+    if (Task.WhenAll(taskList).IsCompleted)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("All files downloaded!!!");
+        Console.ResetColor();
+    }
+    else
+    {
+        for (int i = 0; i < taskList.Count; i++)
+        {
+            Console.WriteLine(
+                $"File: {imageDownloader.DownloadList[i]}\t\t" +
+                $"Download is completed: {taskList[i].IsCompleted}\t\t" +
+                $"Task ID: {taskList[i].Id}");
+        }
+    }
+}
+
+Console.ReadLine();
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void DisplayStartedMessage(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"{message}");
+    Console.ResetColor();
+}
+void DisplayCompletedMessage(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{message}");
+    Console.ResetColor();
+}
+void DisplayFileAvailable(string message)
+{
+    Console.WriteLine("-------------------------------------------");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"{message}");
+    Console.ResetColor();
+    Console.WriteLine("-------------------------------------------");
+}
+void DisplayFileNotAvailable(string message)
+{
+    Console.WriteLine("-------------------------------------------");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{message}");
+    Console.ResetColor();
+    Console.WriteLine("-------------------------------------------");
+}
+
+/*
+List<string> GetUrlListFromTxtFile(string puth)
+{
+    var urlList = new List<string>();
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine($"File: {puth}");
+
+    if (File.Exists(puth))
+    {
+        Console.WriteLine("File available");
+        using (StreamReader reader = new StreamReader(puth))
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                urlList.Add(line);
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("File not available!");
+    }
+    Console.WriteLine("--------------------------------------------");
+
+    return urlList; 
+}
+*/
